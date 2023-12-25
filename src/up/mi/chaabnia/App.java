9c709fede8e65f1f406bbcf7bb83d5f187c1520d
akramch <akram.chaabnia25@gmail.com>
2023-12-25 21:49:14 +0100

    first commit

---
+++ src/up/mi/chaabnia/App.java
@@ -0,0 +1,150 @@
+package up.mi.chaabnia;
+
+import up.mi.chaabnia.WeightedGraph.Edge;
+import up.mi.chaabnia.WeightedGraph.Graph;
+import up.mi.chaabnia.WeightedGraph.Vertex;
+
+import java.util.*;
+
+public class App {
+	private static double findDiameter(Graph graph, int num_v) {
+		double maxDiameter = Double.MIN_VALUE;
+		boolean[] visited = new boolean[num_v];
+
+		for (int i = 0; i < num_v; i++) {
+			if (!visited[i]) {
+				double diameter = calculateComponentDiameter(graph, i, num_v);
+				if (diameter > maxDiameter) {
+					maxDiameter = diameter;
+				}
+			}
+		}
+
+		return maxDiameter;
+	}
+
+	private static double calculateComponentDiameter(Graph graph, int start, int num_v) {
+		double maxDistance = Double.MIN_VALUE;
+
+		for (int i = 0; i < num_v; i++) {
+			LinkedList<Integer> path = Dijkstra(graph, start, i, num_v);
+
+			if (path != null) {
+				// Found a path from 'start' to 'i', update maxDistance if needed
+				double pathDistance = calculatePathDistance(graph, path);
+				if (pathDistance > maxDistance) {
+					maxDistance = pathDistance;
+					System.out.println("Path from " + start + " to " + i + ": " + path.toString());
+					System.out.println("Distance: " + pathDistance);
+
+				}
+			} else {
+				System.out.println("No path found from " + start + " to " + i + ".");
+			}
+
+			resetGraph(graph);
+		}
+
+		return maxDistance;
+	}
+
+	private static double calculatePathDistance(Graph graph, LinkedList<Integer> path) {
+		double distance = 0.0;
+
+		for (int i = 0; i < path.size() - 1; i++) {
+			int current = path.get(i);
+			int next = path.get(i + 1);
+
+			for (Edge edge : graph.vertexlist.get(current).adjacencylist) {
+				if (edge.destination == next) {
+					distance += edge.weight;
+					break;
+				}
+			}
+		}
+
+		return distance;
+	}
+
+	private static void resetGraph(Graph graph) {
+		for (Vertex v : graph.vertexlist) {
+			v.timeFromSource = Double.POSITIVE_INFINITY;
+			v.prev = null;
+		}
+	}
+
+	private static LinkedList<Integer> Dijkstra(Graph graph, int start, int end, int numberV) {
+		PriorityQueue<Vertex> to_visit = new PriorityQueue<>(numberV, Comparator.comparingDouble(v -> v.timeFromSource));
+
+		for (int i = 0; i < numberV; i++) {
+			graph.vertexlist.get(i).timeFromSource = Double.MAX_VALUE;
+			to_visit.add(graph.vertexlist.get(i));
+		}
+
+		graph.vertexlist.get(start).timeFromSource = 0;
+
+		int number_tries = 0;
+
+		while (!to_visit.isEmpty()) {
+			Vertex min_v = to_visit.poll();
+			if (min_v == null) {
+				System.out.println("No minimum vertex found!");
+				break;
+			}
+
+			number_tries++;
+
+			for (Edge edge : min_v.adjacencylist) {
+				int to_try = edge.destination;
+				double new_dist = min_v.timeFromSource + edge.weight;
+
+				if (new_dist < graph.vertexlist.get(to_try).timeFromSource) {
+					graph.vertexlist.get(to_try).timeFromSource = new_dist;
+					graph.vertexlist.get(to_try).prev = min_v;
+
+					to_visit.remove(graph.vertexlist.get(to_try));
+					to_visit.add(graph.vertexlist.get(to_try));
+				}
+			}
+		}
+
+		LinkedList<Integer> path = new LinkedList<>();
+		path.addFirst(end);
+		int current = end;
+		while (current != start) {
+			Vertex prevVertex = graph.vertexlist.get(current).prev;
+			if (prevVertex != null) {
+				current = prevVertex.num;
+				path.addFirst(current);
+			} else {
+				break;
+			}
+		}
+		System.out.println("Path from " + start + " to " + end + ": " + path.toString());
+
+		return path;
+	}
+
+	public static void main(String[] args) {
+		Graph graph = new Graph();
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addVertex(10);
+		graph.addEdge(0, 1, 4);
+		graph.addEdge(0, 2, 3);
+		graph.addEdge(1, 3, 2);
+		graph.addEdge(1, 2, 5);
+		graph.addEdge(2, 3, 7);
+		graph.addEdge(3, 4, 2);
+		graph.addEdge(4, 0, 4);
+		graph.addEdge(4, 1, 4);
+		graph.addEdge(4, 5, 6);
+
+		double diameter = findDiameter(graph, graph.num_v);
+		System.out.println("Diameter of the graph: " + diameter);
+	}
+
+}
